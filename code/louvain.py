20540a8e4b6a22c507ff5ae11f7dcdc5cc30f561
Ashish Dandekar <add.harry@gmail.com>
2015-04-07 16:25:06 +0800

    Corrected error in weighted graph gen

--- code/louvain.py
+++ code/louvain.py
@@ -5,7 +5,7 @@ import sys
 def read_graph(fname):
 	g = nx.Graph()
 	with open(fname, "r") as f:
-		edges = map(lambda x: (x[0], x[1], float(x[2])), (line.strip().split(',') for line in f))
+		edges = map(lambda x: (x[0], x[1], {"weight":float(x[2])}), (line.strip().split(',') for line in f))
 	g.add_edges_from(edges)
 
 	return g
@@ -13,12 +13,12 @@ def read_graph(fname):
 if __name__ == "__main__":
 	G = read_graph(sys.argv[1])
 
-	dendro = community.generate_dendrogram(G)
+	#dendro = community.generate_dendrogram(G)
 
-	for i in range(len(dendro)):
-		print "Partition at level:"
-		# Returns dictionary
-		print community.partition_at_level(dendro, i)
-		print "Modularity:", community.modularity(dendro[i])
+	#for i in range(len(dendro)):
+	#	print "Partition at level:"
+	#	# Returns dictionary
+	#	print community.partition_at_level(dendro, i)
+	#	print "Modularity:", community.modularity(dendro[i])
 
 	

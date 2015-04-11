6cd42ed8635cab5795415a3719d04dc250a13d63
acreux <antoine.creux@gmail.com>
2015-04-11 12:49:31 +0800

    Almost complete reviews processing engine

--- code/algo/reviews.py
+++ code/algo/reviews.py
@@ -1,43 +1,121 @@
 from collections import defaultdict
 import cPickle as pickle
+import fileinput
+from itertools import combinations
+import heapq
+from operator import itemgetter
+
+
 
 class Reviews(object):
 
-    def __init__(self, reviews_csv="reviews.csv"):
-        with open(reviews_csv, "r") as f:
-            self._generate_user_to_book(f)
-
+    def __init__(self):
+        self._book_to_user = None
+        self._user_to_book = None
+        
     @staticmethod
-    def load(filename="reviews_py"):
+    def load(filename="reviews_pickle"):
         """Load a reviews picke file"""
         with open(filename, "rb") as f:
             return pickle.load(f)
-    
-    def _generate_user_to_book(self, f):
 
-        f.seek(0)
-        books_set = set((e.rstrip().split(";")[1] for i, e in enumerate(f)))
-        book_index = {e:i for i, e in enumerate(list(books_set))}
+    def load_csv(self, filename="reviews.csv"):
+        self._generate_user_to_book(f)
 
-        f.seek(0)
-        users_set = set((e.rstrip().split(";")[0] for i, e in enumerate(f)))
-        user_index = {e:i for i, e in enumerate(list(users_set))}
-
-        f.seek(0)
-        self.user_to_book = defaultdict(set)
-        for line in f:
-            user, book, rating = line.rstrip().split(";")
-            self.user_to_book[user_index[user]].add(book_index[book])
-    
-    def book_to_user(self):
-        book_to_user = defaultdict(set)
-
-        for user, book_list in self.user_to_book.iteritems():
-            for book in book_list:
-                book_to_user[book].add(user)
-        return book_to_user
-
-    def save(self, filename="reviews_py"):
-        """Save a graph using pickle"""
+    def save(self, filename="reviews_pickle"):
         with open(filename, "wb") as f:
             pickle.dump(self, f)
+
+    def save_user_to_book(self, filename="user_to_book_pickle"):
+        with open(filename, "wb") as f:
+            pickle.dump(self.user_to_book, f)
+
+    def _generate_user_to_book(self, filename="reviews.csv"):
+        user_to_book = defaultdict(set)
+        with open(filename, "r") as f:
+            for line in f:
+                book, user, rating = line.rstrip().split(";")
+                user_to_book[user].add((book, rating))
+        return user_to_book
+
+    @property
+    def user_to_book(self):
+        if not self._user_to_book:
+            self._user_to_book = self._generate_user_to_book()
+        return self._user_to_book
+
+    def save_book_to_user(self, filename="book_to_user_pickle"):
+        with open(filename, "wb") as f:
+            pickle.dump(self.book_to_user, f)
+
+    def clean_reviews(self, filename="reviews.csv"):
+        book_cpt, last_book_id = 0, ""
+        user_cpt, last_user_id = 0, ""
+
+        f = fileinput.FileInput(filename, inplace=True)
+        for line in f:
+            new_book_id, new_user_id, rating = line.rstrip().split(";")
+            if new_book_id != last_book_id:
+                book_cpt += 1
+                last_book_id = new_book_id
+
+            if new_user_id != last_user_id:
+                user_cpt += 1
+                last_user_id = new_user_id
+
+            print ";".join([str(book_cpt), str(user_cpt), rating])
+        f.close()
+    
+    @property
+    def book_to_user(self):
+        if not self._book_to_user:
+            self._book_to_user = self._generate_book_to_user()
+        return self._book_to_user
+
+    def _generate_book_to_user(self, filename="reviews.csv"):
+
+        book_to_user = defaultdict(set)
+        with open(filename, "r") as f:
+            for line in f:
+                book, user, rating = line.rstrip().split(";")
+                book_to_user[book].add((user, rating))
+        return book_to_user
+
+    def user_to_user(self, reviews_filename="reviews.csv"):
+
+        def basic_score(rating1, rating2):
+            """Score between two users. Only based on the ratings"""
+            return rating1 + rating2
+
+        def mix(user_rating_set):
+            for i, j in combinations(user_rating_set, 2):
+                u1, r1 = i
+                u2, r2 = j
+                yield u1, r1, u2, r2, basic_score(int(r1), int(r2))
+
+        def score_gen():
+            with open(reviews_filename) as f:
+
+                last_book_id = ""
+                book_bucket = set()
+
+                i = 0
+                for line in f:
+                    i += 1
+                    if not i % 10**5:
+                        print i
+                    book, user, rating = line.rstrip().split(";")
+
+                    if book == last_book_id:
+                        book_bucket.add((user, rating))
+                    else:
+                        for edge in mix(book_bucket): yield edge
+                        last_book_id = book
+
+
+                else:
+                    for edge in mix(book_bucket): yield edge
+
+        return heapq.nlargest(10, score_gen(), key=itemgetter(4))
+
+
---
+++ code/louvain.py
@@ -0,0 +1,24 @@
+import community
+import networkx as nx
+import sys
+
+def read_graph(fname):
+	g = nx.Graph()
+	with open(fname, "r") as f:
+		edges = map(lambda x: (x[0], x[1], {"weight":float(x[2])}), (line.strip().split(',') for line in f))
+	g.add_edges_from(edges)
+
+	return g
+
+if __name__ == "__main__":
+	G = read_graph(sys.argv[1])
+
+	#dendro = community.generate_dendrogram(G)
+
+	#for i in range(len(dendro)):
+	#	print "Partition at level:"
+	#	# Returns dictionary
+	#	print community.partition_at_level(dendro, i)
+	#	print "Modularity:", community.modularity(dendro[i])
+
+	
